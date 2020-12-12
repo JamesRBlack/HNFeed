4e7410e27da167a4395369a536cc9df8a9f6fc3d
James Black <james.black@urbanutilities.com.au>
2020-12-12 19:31:43 +1000

    debounced search

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -10,15 +10,22 @@ export class SearchBarComponent implements OnInit {
 
   @Output() searchEvent = new EventEmitter<string>();
   searchQuery = '';
+  previousSearch = '';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  search(): any {
-    this.searchEvent.emit(`/search?query=${this.searchQuery}&tags=story`);
+  checkCanSearch(): boolean {
+    return (this.previousSearch === '' || (this.previousSearch !== this.searchQuery));
   }
 
+  search(): any {
+    if (this.checkCanSearch()) {
+      this.searchEvent.emit(`/search?query=${this.searchQuery}&tags=story`);
+      this.previousSearch = this.searchQuery;
+    }
+  }
 
 }
