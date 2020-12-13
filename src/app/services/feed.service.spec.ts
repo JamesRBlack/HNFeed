2a0372281af9f02d96bb8ae5481bf2434a56f129
James Black <james.black@urbanutilities.com.au>
2020-12-13 15:19:27 +1000

    got service test working

--- src/app/services/feed.service.spec.ts
+++ src/app/services/feed.service.spec.ts
@@ -2,6 +2,10 @@ import { TestBed } from '@angular/core/testing';
 
 import { FeedService } from './feed.service';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {of} from 'rxjs';
+import {Feed} from '../model/feed';
+import {FeedViewComponent} from '../components/feed-view/feed-view.component';
+import {AppComponent} from '../app.component';
 
 describe('FeedService', () => {
   let service: FeedService;
@@ -13,5 +17,14 @@ describe('FeedService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return HN feed results', () => {
+      const data: any = require('../../assets/Mock/test-data.json');
+      spyOn(service, 'getPosts').and.returnValue(of(data));
+
+      const feedView = new AppComponent(service);
+      feedView.getSearchedPosts('test');
+      expect(feedView.posts.length).toEqual(20);
   });
 });
