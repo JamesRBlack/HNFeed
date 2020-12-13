0d2a02a7b638a512e47b4d11b1cd6ce0f4c25224
James Black <james.black@urbanutilities.com.au>
2020-12-13 13:43:51 +1000

    got Karma tests working

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
-      ],
+        AppComponent,
+      ], imports: [HttpClientTestingModule]
     }).compileComponents();
   });
 
@@ -20,12 +21,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('HNFeed');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('HNFeed app is running!');
   });
 });
--- src/app/components/feed-view/feed-view.component.spec.ts
+++ src/app/components/feed-view/feed-view.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FeedViewComponent } from './feed-view.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('FeedViewComponent', () => {
   let component: FeedViewComponent;
@@ -8,7 +9,7 @@ describe('FeedViewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FeedViewComponent ]
+      declarations: [ FeedViewComponent  ], imports: [HttpClientTestingModule]
     })
     .compileComponents();
   });
